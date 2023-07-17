1b8d417187fec38b9d3a0af2923f76cb72f2a19b
hayat <sharghhayat@gmail.com>
2023-07-17 18:58:31 +0330

    next page plaque complited show and route

--- src/app/pages/admin-panel/admin-panel.component.ts
+++ src/app/pages/admin-panel/admin-panel.component.ts
@@ -43,7 +43,7 @@ export class AdminPanelComponent implements OnInit {
     this.showcomponent3 = false;
   }
   clear() {
-    localStorage.clear();
+    localStorage.removeItem("token");
   }
 
   token: any;
